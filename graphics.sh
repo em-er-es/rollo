f399280178992fe9650e1899b0b219600d5ad98a
Ernest Skrzypczyk <emeres.code@onet.eu>
2016-01-11 00:27:43 +0100

    Added graphics.sh precompile script

--- graphics.sh
+++ graphics.sh
@@ -1,1 +1,16 @@
-/home/emeres/.bin/latex-precompile--graphics
+#!bin/bash
+DIRG="Graphics";
+EXT1="svg";
+EXT2="pdf";
+DPI=96;
+TOL=10;
+
+for FILE in "$DIRG/"*".$EXT1"; do
+	FILE2="$(echo "$FILE" | sed "s/$EXT1\$/$EXT2/g")";
+	MODTIME1=$(stat -c %Y "$FILE");
+	MODTIME2=$(stat -c %Y "$FILE2");
+	MODDIF=$((MODTIME1 - MODTIME2));
+	if [[ $MODDIF -gt $TOL ]]; then
+		inkscape -z -D --file="$FILE" --export-pdf="$FILE2" --export-dpi=$DPI --export-latex
+	fi
+done
