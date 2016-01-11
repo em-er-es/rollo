10b3747c906cf4b2c8b7c0885958a848405c5929
Ernest Skrzypczyk <emeres.code@onet.eu>
2016-01-11 20:32:40 +0100

    Changed the cruderollo communication protocol for independent wheel control

--- Applications/cruderollo/cruderollo.sh
+++ Applications/cruderollo/cruderollo.sh
@@ -26,7 +26,7 @@ exit 0;
 help_rollo(){
 echo -e "\nRollo crude control script\nCommands:\n";
 echo -e "MOV:[fF]orward\t[bB]ackward\t[lL]eft\t[rR]ight\n[sS]top\t\tre[C]onnect\t[Q]uit\n";
-echo -e "VEL:[0]* = 0%\t[1]* = 12%\t[2]* = 25%\t[3]* = 37%\n[4]* = 50%\t[5]* = 62%\t[6]* = 75%\t[7]* = 87%\n\t\t\t\t\t\t[8|9]* = 100%\n";
+echo -e "VEL:[0]* = 6%\t[1]* = 12%\t[2]* = 19%\t[3]* = 25%\n[4]* = 31%\t[5]* = 38%\t[6]* = 44%\t[7]* = 50%\n\t\t\t\t\t\t[8|9]* = 56%\n";
 #echo -e "\n";
 }
 
@@ -34,18 +34,14 @@ help_rollo;
 connect_rollo;
 #echo -e "Ready."
 echo -e "Input:"
-MSG3=31;
+#MSG3=31;
 STOP=0;
 TMP=$ROLLO_REPEAT_CMD;
 COUNTER=1;
 while :; do
-#	read CMD
 	echo -e "\nMOV:"
 	read -n1 CMD1
-#	MOV=${CMD[1]}; VEL=${CMD[2]};
 	MOV=$CMD1;
-#	echo $MOV $VEL
-#	echo $CMD1 $CMD2
 	case "$MOV" in
 		[fF]*)
 		MSG1=7c;
@@ -92,13 +88,13 @@ while :; do
 		;;
 	esac
 	if [[ $STOP == 0 ]]; then
-	echo -e "\nVEL:";
-	read -n1 CMD2
-	VEL=$CMD2;
-	case "$VEL" in
+	WHEEL="LEFT";
+	echo -e "\n$WHEEL VEL:";
+	read -n1 VELL
+	case "$VELL" in
 		0*)
 		MSG2=50;
-		INFO2="0%";
+		INFO2="6%";
 		;;
 		1*)
 		MSG2=55;
@@ -106,31 +102,31 @@ while :; do
 		;;
 		2*)
 		MSG2=56;
-		INFO2="25%";
+		INFO2="19%";
 		;;
 		3*)
 		MSG2=57;
-		INFO2="37%";
+		INFO2="25%";
 		;;
-		[45]*)
+		4*)
 		MSG2=59;
-		INFO2="50%";
+		INFO2="31%";
+		;;
+		5*)
+		MSG2=5F;
+		INFO2="38%";
 		;;
 		6*)
-		MSG2=5F;
-		INFO2="62%";
+		MSG2=60;
+		INFO2="44%";
 		;;
 		7*)
-		MSG2=60;
-		INFO2="75%";
-		;;
-		8*)
 		MSG2=61;
-		INFO2="87%";
+		INFO2="50%";
 		;;
-		9*)
+		[89]*)
 		MSG2=62;
-		INFO2="100%";
+		INFO2="56%";
 		;;
 #		10*)
 #		MSG2=62;
@@ -139,9 +135,8 @@ while :; do
 #		[qQeExX]*)
 		[sS]*)
 		MSG1=7b;
-		MSG2=50;
 		INFO1="STOP";
-		INFO2="0%";
+		INFO2="L $MSG2% R $MSG3%";
 		STOP=1;
 		;;
 		[Q]*)
@@ -164,6 +159,77 @@ while :; do
 		continue;
 		;;
 	esac
+	WHEEL="RIGHT";
+	echo -e "\n$WHEEL VEL:";
+	read -n1 VELR
+	case "$VELR" in
+		0*)
+		MSG3=10;
+		INFO2="6%";
+		;;
+		1*)
+		MSG3=11;
+		INFO2="12%";
+		;;
+		2*)
+		MSG3=12;
+		INFO2="19%";
+		;;
+		3*)
+		MSG3=13;
+		INFO2="25%";
+		;;
+		4*)
+		MSG3=1A;
+		INFO2="31%";
+		;;
+		5*)
+		MSG3=1B;
+		INFO2="38%";
+		;;
+		6*)
+		MSG3=1C;
+		INFO2="44%";
+		;;
+		7*)
+		MSG3=1D;
+		INFO2="50%";
+		;;
+		[89]*)
+		MSG3=24;
+		INFO2="56%";
+		;;
+#		10*)
+#		MSG3=62;
+#		INFO2="100%";
+#		;;
+#		[qQeExX]*)
+		[sS]*)
+		MSG1=7b;
+		INFO1="STOP";
+		INFO2="L $MSG2% R $MSG3%";
+		STOP=1;
+		;;
+		[Q]*)
+#		echo -e "\nExiting."
+		exit_rollo;
+		;;
+		C*)
+		echo -e "\nReconnecting."
+		connect_rollo;
+		continue;
+		;;
+		[hH]*)
+#		echo -e "\nPrint help."
+		help_rollo;
+		;;
+		*)
+		echo -e "\nParamter 3 not understood. Repeat"
+#		echo -e "\nParamter 3 not understood. Exiting"
+#		exit 1;
+		continue;
+		;;
+	esac
 	fi
 	echo -e "\nRollo: $INFO1 @ $INFO2 speed";
 	if [[ $STOP ]] && [[ $ROLLO_REPEAT_CMD < 6 ]]; then TMP=$ROLLO_REPEAT_CMD; ROLLO_REPEAT_CMD=6; fi
@@ -173,6 +239,7 @@ while :; do
 	for i in $(seq $ROLLO_REPEAT_CMD); do udp_rollo 0x$MSG1$MSG2$MSG3 &> /dev/null; done
 	fi
 	unset MSG2;
+	unset MSG3;
 	if [[ $STOP ]]; then ROLLO_REPEAT_CMD=$TMP; fi
 	STOP=0;
 	echo Counter: $COUNTER;
