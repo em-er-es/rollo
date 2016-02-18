810332b92e6675ada7a8f93b5bc67fc3dcb987ff
Ernest Skrzypczyk <emeres.code@onet.eu>
2016-02-18 07:04:12 +0100

    Added header file for all nodes.

---
+++ rollo/src/rollo_nodes.h
@@ -0,0 +1,23 @@
+// Node names
+#define PP "PREP"
+#define LC "LOC "
+#define KF "EKF "
+#define CT "CTRL"
+#define OD "ODOM"
+#define CM "COMM"
+
+// GNU/Linux terminal color codes
+#define CR "\033[0m"
+
+#define C1 "\033[38;5;63m"
+#define C2 "\033[38;5;220m"
+#define C3 "\033[38;5;87m"
+#define C4 "\033[38;5;84m"
+#define C5 "\033[38;5;160m"
+#define C6 "\033[38;5;161m"
+#define C7 "\033[38;5;162m"
+#define C8 "\033[38;5;22m"
+
+#define CEE "\033[38;5;124m" /* Error */
+#define CSS "\033[38;5;154m" /* Success */
+#define CWW "\033[38;5;202m" /* Warning */
